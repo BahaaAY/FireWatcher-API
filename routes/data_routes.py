94c60f06c26cbc7e2555c9bff53a71c3c4f68aef
BahaaAY <bahaa.abouyassine2@gmail.com>
2024-06-02 18:06:08 +0300

    fix algorithm

--- routes/data_routes.py
+++ routes/data_routes.py
@@ -47,11 +47,12 @@ async def save_readings(node_data: Data):
     last_node_reading = await data_collection.find_one({"node_id": search_id,"updated_FWI":True}, sort=[("timestamp", -1)])
     print("last node reading: ", last_node_reading)
     
+    
     current_date = datetime.datetime.now()
     noon_time = current_date.replace(hour=12, minute=0, second=0, microsecond=0)
     start_window = noon_time - timedelta(minutes=30)
     end_window = noon_time + timedelta(minutes=30)
-
+    updated_fwi = False
     if last_node_reading is not None:
         last_reading_time = last_node_reading["timestamp"]  # Ensure this is a datetime object
         time_difference = current_date - last_reading_time
@@ -59,33 +60,11 @@ async def save_readings(node_data: Data):
     # Check if 24 hours have passed since the last update and if current time is around 12 noon
         if time_difference > timedelta(days=1) and (start_window <= current_date <= end_window):
         # Conditions met, proceed with updating
-        # Your logic to calculate and update FFMC, DMC, DC, etc.
-            weather_data = await fetch_weather_data(node["latitude"], node["longitude"])
-            print(weather_data)
-            mth = int(current_date.month)
-            fwisystem = FWIClass(node_data.temperature,node_data.humidity,weather_data["current"]["wind_kph"],weather_data["current"]["precip_mm"])
-            # fwisystem = FWIClass(node_data.temperature,node_data.humidity,weather_data["current"]["wind_kph"],12)
-            ffmc = fwisystem.FFMCcalc(ffmc0)
-            dmc = fwisystem.DMCcalc(dmc0,mth)
-            dc = fwisystem.DCcalc(dc0,mth)
-            isi = fwisystem.ISIcalc(ffmc)
-            bui = fwisystem.BUIcalc(dmc,dc)
-            fwi = fwisystem.FWIcalc(isi,bui)
-            ffmc0 = ffmc
-            dmc0 = dmc
-            dc0 = dc
-    
-        else:
-        # Conditions not met, do not update FFMC, DMC, DC
-        # You can continue to use the last stored values
-            print("found reading with timestamp: ", last_node_reading["timestamp"])
+        # USE FFMC0 from the last reading to calculate new FFMC, DMC, DC
+
             ffmc0 = last_node_reading["ffmc"]
             dmc0 = last_node_reading["dmc"]
             dc0 = last_node_reading["dc"]
-    else:
-        # No previous reading, check if it's around 12 noon to proceed
-        if start_window <= current_date <= end_window:
-        # Your logic to calculate and update FFMC, DMC, DC, etc. for the first time
             weather_data = await fetch_weather_data(node["latitude"], node["longitude"])
             print(weather_data)
             mth = int(current_date.month)
@@ -97,12 +76,25 @@ async def save_readings(node_data: Data):
             isi = fwisystem.ISIcalc(ffmc)
             bui = fwisystem.BUIcalc(dmc,dc)
             fwi = fwisystem.FWIcalc(isi,bui)
-            ffmc0 = ffmc
-            dmc0 = dmc
-            dc0 = dc
+            updated_fwi = True
+            
+    
         else:
-        # Not around noon, you may choose to wait or handle this case differently
-        # get the last FFMC readings
+        # Conditions not met, do not update FFMC, DMC, DC -> dont calculate ffm, dmc, dc and isi, bui, fwi
+        # You can continue to use the last stored values
+            print("found reading with timestamp: ", last_node_reading["timestamp"])
+            ffmc = last_node_reading["ffmc"]
+            dmc = last_node_reading["dmc"]
+            dc = last_node_reading["dc"]
+            isi = last_node_reading["isi"]
+            bui = last_node_reading["bui"]
+            fwi = last_node_reading["fwi"]
+            updated_fwi = False
+
+    else:
+        # No previous reading, check if it's around 12 noon to proceed
+        if start_window <= current_date <= end_window:
+        # it is around noon, calculate new FFMC, DMC, DC that will be saved (using default values)
             ffmc0 = 85.0
             dmc0 = 6.0
             dc0 = 15.0
@@ -117,9 +109,26 @@ async def save_readings(node_data: Data):
             isi = fwisystem.ISIcalc(ffmc)
             bui = fwisystem.BUIcalc(dmc,dc)
             fwi = fwisystem.FWIcalc(isi,bui)
-            ffmc0 = ffmc
-            dmc0 = dmc
-            dc0 = dc
+            updated_fwi = True
+            
+
+        else:
+        # Not around noon  calculate new FFMC, DMC, DC that  will be used for calculations but not saved
+            ffmc0 = 85.0
+            dmc0 = 6.0
+            dc0 = 15.0
+            weather_data = await fetch_weather_data(node["latitude"], node["longitude"])
+            print(weather_data)
+            mth = int(current_date.month)
+            fwisystem = FWIClass(node_data.temperature,node_data.humidity,weather_data["current"]["wind_kph"],weather_data["current"]["precip_mm"])
+            # fwisystem = FWIClass(node_data.temperature,node_data.humidity,weather_data["current"]["wind_kph"],12)
+            ffmc = fwisystem.FFMCcalc(ffmc0)
+            dmc = fwisystem.DMCcalc(dmc0,mth)
+            dc = fwisystem.DCcalc(dc0,mth)
+            isi = fwisystem.ISIcalc(ffmc)
+            bui = fwisystem.BUIcalc(dmc,dc)
+            fwi = fwisystem.FWIcalc(isi,bui)
+            updated_fwi = False
    
     voting_clf = load(model_path)
 
@@ -143,6 +152,7 @@ async def save_readings(node_data: Data):
     #convert to boolean
     predicted_output = [bool(x) for x in predicted_output]
 
+     
     # Insert the data into the database
 
     data = DataSchema(
@@ -159,7 +169,8 @@ async def save_readings(node_data: Data):
         bui=bui,
         fwi=fwi,
         fire_risk=predicted_output[0],
-        fire=True if node_data.smoke_value >= 1100 else False
+        fire=True if node_data.smoke_value >= 1100 else False,
+        updated_FWI=updated_fwi
     )
 
     await insert_data(data)
