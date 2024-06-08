f3635eb10916e9cf4e542a4aae14aeeb681c66b7
Omar Halanlab <omarhalablab72@gmail.com>
2024-06-08 18:37:52 +0300

    added cors policy

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
 from routes.node_routes import router as node_router
 from routes.data_routes import router as data_router
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+    
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 
 app.include_router(node_router)
 app.include_router(data_router)
